f3b515e1fea795bd04fccb72c26759a71b09964b
iStork1 <f.jaimes@uniandes.edu.co>
2023-04-01 19:08:51 -0500

    Clase Grupo y Tarifas_Habitación

---
+++ src/Logica/Grupo.java
@@ -0,0 +1,14 @@
+package Logica;
+
+import java.util.ArrayList;
+
+public abstract class Grupo{
+	ArrayList<Cliente> listaClientes = new ArrayList<Cliente>();
+	
+	public void editarHistorialGrupo () {
+
+	}
+	public void generarFacturaGrupo() {
+
+	}
+}
---
+++ src/Logica/Tarifas_Habitación.java
@@ -0,0 +1,16 @@
+package Logica;
+
+import java.util.ArrayList;
+
+public abstract class Tarifas_Habitación{
+	protected String tipo; //No se a que se refiere con tipo
+	protected String fechaInicio;
+	protected String fechaFin;
+	protected float precio;
+	ArrayList<String> diasSemana = new ArrayList<String>();
+	
+	public Tarifas_Habitación() {
+
+	}
+    
+}
