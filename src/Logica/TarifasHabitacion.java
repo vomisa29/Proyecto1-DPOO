6c7b99a7a84e54c0f6a2fac426f9e93ebb1c9fe0
Pablo_M <p.mendezm@uniandes.edu.co>
2023-04-01 20:42:43 -0500

    cambios menores

--- src/Logica/TarifasHabitacion.java
+++ src/Logica/TarifasHabitacion.java
@@ -2,14 +2,14 @@ package Logica;
 
 import java.util.ArrayList;
 
-public abstract class Tarifas_Habitación{
+public abstract class TarifasHabitacion{
 	protected String tipo; //No se a que se refiere con tipo
 	protected String fechaInicio;
 	protected String fechaFin;
 	protected float precio;
 	ArrayList<String> diasSemana = new ArrayList<String>();
 	
-	public Tarifas_Habitación() {
+	public void Tarifas_Habitación() {
 
 	}
     
