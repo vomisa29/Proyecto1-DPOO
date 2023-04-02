2f35c64cd849fe06e0f7f6edab45eb51b02e98cf
Pablo_M <p.mendezm@uniandes.edu.co>
2023-04-02 17:55:58 -0500

    .

--- src/Logica/Hotel.java
+++ src/Logica/Hotel.java
@@ -3,9 +3,15 @@ package Logica;
 import java.util.ArrayList;
 
 public class Hotel {
-	ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
-	public Hotel(){
-		
+	protected ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+	protected ArrayList<Factura> facturas = new ArrayList<Factura>();
+	protected Servicios servicios;
+	
+	public Hotel(ArrayList<Usuario> usuarios, ArrayList<Factura> facturas, Servicios servicios) {
+		super();
+		this.usuarios = usuarios;
+		this.facturas = facturas;
+		this.servicios = servicios;
 	}
 
 }
