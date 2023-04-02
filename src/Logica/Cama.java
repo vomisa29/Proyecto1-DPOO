5a4ac5fda2d2192a96483fe6de8700ddb0b3e8ae
Pablo_M <p.mendezm@uniandes.edu.co>
2023-04-02 18:36:28 -0500

    .

--- src/Logica/Cama.java
+++ src/Logica/Cama.java
@@ -4,8 +4,6 @@ public class Cama {
 	protected int capacidadCama;
 	protected String tipoCama;// puede ser Estandar, Suite o Suite Doble
 	protected boolean soloNinos = false;
-	protected int[] precios = new int[3];
-	// pos 0: precio estandar, pos 1: precio suite, pos 2: precio suite doble
 	
 	public Cama(int capacidadCama, String tipoCama, boolean soloNinos) {
 		this.capacidadCama = capacidadCama;
@@ -30,20 +28,5 @@ public class Cama {
 	public void setSoloNinos(boolean soloNinos) {
 		this.soloNinos = soloNinos;
 	}
-	
-	protected int calcularPrecioCama() {
-		int precio = 0;
-		if (this.tipoCama.equals("Estandar")) {
-			precio += this.precios[0];	
-		}else if (this.tipoCama.equals("Suite")) {		
-			precio += this.precios[1];		
-		}else if(this.tipoCama.equals("Suite Doble")) {		
-			precio += this.precios[2];
-		}
-		
-	return precio;
-	}
-	
-	
 
 }
